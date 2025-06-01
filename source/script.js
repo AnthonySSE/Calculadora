2ea02bc698d1b5110ed53123257b081f1e8b2732
AnthonySSE <anthonyasse17@gmail.com>
2025-06-01 18:36:25 -0400

    Avanaces
    Ciertas configuraciones de comportamientos.

--- source/script.js
+++ source/script.js
@@ -9,7 +9,35 @@ display.addEventListener("input", function () {
   }
 });
 
-//Separando los valores con ,
+//Colocando el cursor al final del display
+
+//Al enfocar
+display.addEventListener("focus", function () {
+  const length = this.value.length;
+  this.setSelectionRange(length, length);
+});
+
+//Al hacer click
+display.addEventListener("mouseup", function (e) {
+  e.preventDefault(); // Previene que el navegador cambie la posición del cursor
+  const length = this.value.length;
+  this.setSelectionRange(length, length);
+});
+
+//Eliminar el valor de 0 si se pulsa cualquier numero.
+display.addEventListener("input", function () {
+  if (this.value.length > 1 && this.value.startsWith("0")) {
+    this.value = this.value.slice(1); // Elimina el 0 inicial
+  }
+});
+
+//Solo permitir ingresar numeros.
+display.addEventListener("input", function (e) {
+  const key = e.key;
+  if (key < "0" || key > "9") {
+    e.preventDefault();
+  }
+});
 
 //Obteniendo el valor de los botones y añadiendolos al display.
 buttons.map((button) => {
